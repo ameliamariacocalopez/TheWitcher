ba8599a00f4e26f65ee10a3012cf044ade4f1584
amelia <ameliamariacocalopez@gmail.com>
2023-01-11 12:40:10 +0100

    añadimos atributos

--- src/witcher/Witcher.java
+++ src/witcher/Witcher.java
@@ -1,7 +1,9 @@
 package witcher;
 
 public class Witcher {
-
+	private String name;
+	private int health;
+	private boolean hasSoul;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
