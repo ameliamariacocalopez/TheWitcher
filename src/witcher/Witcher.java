693bb9c68f7855eb80cfe0943f74053ca989b935
amelia <ameliamariacocalopez@gmail.com>
2023-01-18 12:36:29 +0100

    Añadiendo Main

--- src/witcher/Witcher.java
+++ src/witcher/Witcher.java
@@ -36,24 +36,9 @@ public class Witcher {
 	}
 }
 	
-	public static void main(String[] args) {
-	// TODO Auto-generated method stub
-	Witcher brujo = new Witcher();
-	brujo.setName("Geralt de Rivia");
-	brujo.setHealth(15);
-	brujo.setHasSoul(false) ;
-	if(brujo.isDeath()) {
-	System.out.println(brujo.getName() + " esta muerto");
-	}else {
-	System.out.println(brujo.getName() + " esta vivo");
 	
-	}
-	}
 
-	
 	private int health;
 	private boolean hasSoul;
 	
-	
-
 }
